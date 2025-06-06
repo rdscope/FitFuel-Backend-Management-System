169b9d83377dafefe465e8ebf89230ae54a4198d
Jeff Hsu <rdsc.dev@gmail.com>
2025-06-07 02:50:58 +0800

    update unfullfilled: 00

--- src/dao/ProductDao.java
+++ src/dao/ProductDao.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface ProductDao extends DAO<Product> {
-    List<Product> findByCategory(int categoryId) throws SQLException;
+    List<Product> findByCategory(int categoryId) throws SQLException; // --unfullfilled
 
-    List<Product> findLowStock(int threshold) throws SQLException;
+    List<Product> findLowStock(int threshold) throws SQLException; // --unfullfilled
 
     List<Product> findMadeToOrderProducts() throws SQLException;
 }
